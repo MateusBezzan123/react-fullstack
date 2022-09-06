c77d27af586a41f71dd25918c31e11644f496223
Mateus Correia Bezzan <48907717+MateusBezzan123@users.noreply.github.com>
2022-09-05 21:34:58 -0300

    Full Stack Web Development Course [9] | ReactJS, NodeJS, MySQL - JWT Authentication

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,27 +1,39 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassord] = useState("");
 
-    const [username, setUsername] = useState("")
-    const [password, setPassord] = useState("")
+  let history = useHistory();
 
-    const login = () => {
-        const data = { username: username, password: password }
-        axios.post("http://localhost:3001/auth/login", data).then((response)=>{
-            console.log(response.data)
-        })
-    }
+  const login = () => {
+    const data = { username: username, password: password };
+    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+      if (response.data.error) {
+        alert(response.data.error);
+      } else {
+        sessionStorage.setItem("accessToken", response.data);
+        history.push("/")
+      }
+    });
+  };
 
-    return (
-        <div>
-            <input type="text" onChange={(event) => setUsername(event.target.value)} />
-            <input type="password" onChange={(event) => setPassord(event.target.value)} />
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <input
+        type="password"
+        onChange={(event) => setPassord(event.target.value)}
+      />
 
-            <button onClick={login}>Login</button>
-        </div>
-    )
+      <button onClick={login}>Login</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,58 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 
-
 function Post() {
-    let { id } = useParams()
-    const [postObject, setPostObeject] = useState({})
-    const [comments, setComments] = useState([])
-    const [newComent, setNewComment] = useState("")
+  let { id } = useParams();
+  const [postObject, setPostObeject] = useState({});
+  const [comments, setComments] = useState([]);
+  const [newComent, setNewComment] = useState("");
 
-    useEffect(() => {
-        axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
-            setPostObeject(response.data)
-        })
+  useEffect(() => {
+    axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
+      setPostObeject(response.data);
+    });
 
-        axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
-            setComments(response.data)
-        })
-    },[])
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data);
+    });
+  }, []);
 
-    const addComment = () => {
-        axios.post("http://localhost:3001/comments", { commentBody: newComent, PostId: id }).then((response) => {
-            const commentToAdd = {commentBody: newComent}
-            setComments([...comments,commentToAdd])
-            setNewComment("")
-        })
-    }
+  const addComment = () => {
+    axios
+      .post(
+        "http://localhost:3001/comments",
+        { commentBody: newComent, PostId: id },
+        { headers: { accessToken: sessionStorage.getItem("accessToken")} }
+      )
+      .then((response) => {
+        if (response.data.error)
+        {
+            alert(response.data.error)
+        } else {
+            const commentToAdd = { commentBody: newComent };
+            setComments([...comments, commentToAdd]);
+            setNewComment("");
+        }
+      });
+  };
 
-    return (
-        <div className="postPage">
-            <div className="leftSide">
-                <div className="post" id="individual">
-                    <div className="title"> {postObject.title} </div>
-                    <div className="body">{postObject.postText}</div>
-                    <div className="footer">{postObject.username}</div>
-                </div>
-            </div>
-            <div className="rightSide">
-                <div className="addCommentContainer">
-                    <input type="text" placeholder="Comment.." autoComplete="off" onChange={(event) => { setNewComment(event.target.value) }} />
-                    <button onClick={addComment}> Add Comment</button>
-                </div>
-                <div className="listOfComments">
-                    {comments.map((comment, key) => {
-                        return (
-                            <div key={key} className="comment">
-                                {comment.commentBody}
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
+  return (
+    <div className="postPage">
+      <div className="leftSide">
+        <div className="post" id="individual">
+          <div className="title"> {postObject.title} </div>
+          <div className="body">{postObject.postText}</div>
+          <div className="footer">{postObject.username}</div>
         </div>
-    )
+      </div>
+      <div className="rightSide">
+        <div className="addCommentContainer">
+          <input
+            type="text"
+            placeholder="Comment.."
+            autoComplete="off"
+            onChange={(event) => {
+              setNewComment(event.target.value);
+            }}
+          />
+          <button onClick={addComment}> Add Comment</button>
+        </div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return (
+              <div key={key} className="comment">
+                {comment.commentBody}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
